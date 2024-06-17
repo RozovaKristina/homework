b05df9e2744d4ea66ee2488eb57ba1f530a800fc
Kristina Rozova <13rzv13@gmail.comm>
2024-06-17 21:00:07 +0300

    widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,1 +1,14 @@
-from masks import get_mask_account
+from src.masks import get_mask_card_number
+from src.masks import get_mask_account
+
+def mask_account_card(cards_number: str) -> str | None:
+    if "Счет" in cards_number:
+        mask_account = f"Счет {get_mask_account(cards_number[5:])}"
+        return mask_account
+    else:
+        card = get_mask_card_number(cards_number[-16:])
+        mask_card = cards_number.replace(cards_number[-16:], card)
+        return mask_card
+
+def get_data(data: str) -> str | None:
+    return f"{data[8:10]}.{data[5:7]}.{data[0:4]}"
--- tests/tests_demo.py
+++ tests/tests_demo.py
@@ -1,6 +1,13 @@
 from src.masks import get_mask_card_number
 from src.masks import get_mask_account
+from src.widget import mask_account_card
+from src.widget import get_data
 
 print(get_mask_card_number("7000792289606361"))
 
 print(get_mask_account("73654108430135874315"))
+
+print(mask_account_card("Maestro 1596837868705199"))
+print(mask_account_card("Счет 64686473678894779589"))
+
+print(get_data("2018-07-11T02:26:18.671407"))
