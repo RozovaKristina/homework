abf1edcacdd32d93f02298306993403fa9c71f04
Kristina Rozova <13rzv13@gmail.comm>
2024-06-17 18:39:36 +0300

    update gitignore

---
+++ src/masks.py
@@ -0,0 +1,14 @@
+def get_mask_card_number(card_number: str) -> str | None:
+    """принимает на вход номер карты и возвращает ее маску"""
+    if card_number.isdigit() and len(card_number) == 16:
+        return f"{card_number[:5]} {card_number[5:7]}{"*" * 2} {"*" * 4} {card_number[12:]}"
+    else:
+        return None
+
+
+def get_mask_account(acc_number: str) -> str | None:
+    """принимает на вход номер счета и возвращает его маску"""
+    if acc_number.isdigit() and len(acc_number) == 20:
+        return f"{'*' * 2}{acc_number[-4::]}"
+    else:
+        return None
