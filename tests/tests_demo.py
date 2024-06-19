a15124c4f1aa0502016b2550585d67553d5cb776
Kristina Rozova <13rzv13@gmail.comm>
2024-06-19 21:36:49 +0300

    init

--- tests/tests_demo.py
+++ tests/tests_demo.py
@@ -9,6 +9,12 @@ print(get_mask_account("73654108430135874315"))
 
 print(mask_account_card("Maestro 1596837868705199"))
 print(mask_account_card("Счет 64686473678894779589"))
+print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("Счет 35383033474447895560"))
+print(mask_account_card("Visa Classic 6831982476737658"))
+print(mask_account_card("Visa Platinum 8990922113665229"))
+print(mask_account_card("Visa Gold 5999414228426353"))
+print(mask_account_card("Счет 73654108430135874305"))
 
 print(get_data("2018-07-11T02:26:18.671407"))
 
