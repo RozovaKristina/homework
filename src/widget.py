967513fe350234f9707a8fe104398461376b9a73
Kristina Rozova <13rzv13@gmail.comm>
2024-06-23 00:10:02 +0300

    src/widget.py

--- src/widget.py
+++ src/widget.py
@@ -2,7 +2,7 @@ from src.masks import get_mask_card_number
 from src.masks import get_mask_account
 
 
-def mask_account_card(cards_number: str) -> str | None:
+def mask_account_card(cards_number: str) -> str:
     """Функция общей маскировки карты и счета"""
     if "Счет" in cards_number:
         mask_account = f"Счет {get_mask_account(cards_number[5:])}"
@@ -13,6 +13,6 @@ def mask_account_card(cards_number: str) -> str | None:
         return mask_card
 
 
-def get_data(data: str) -> str | None:
+def get_data(data: str) -> str:
     """Функция преобразования даты"""
     return f"{data[8:10]}.{data[5:7]}.{data[0:4]}"
