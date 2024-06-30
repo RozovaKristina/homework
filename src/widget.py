16921a8e257aadb55678c1b1a707e353e344afbf
Kristina Rozova <13rzv13@gmail.comm>
2024-06-30 17:48:09 +0300

    homework_10_2_2

--- src/widget.py
+++ src/widget.py
@@ -2,20 +2,20 @@ from src.masks import get_mask_card_number
 from src.masks import get_mask_account
 
 
-def mask_account_card(cards_number: str) -> str | None:
+def mask_account_card(cards_number: str) -> str:
     """Функция общей маскировки карты и счета"""
-    if "Счет" in cards_number:
-        mask_account = f"Счет {get_mask_account(cards_number[5:])}"
-        return mask_account
-    elif len(cards_number) < 16:
+    if len(cards_number) < 16:
         return "Некорректный ввод"
+
+    if "Счет" in cards_number:
+        masked_payment = f"Счет {get_mask_account(cards_number[5:])}"
     else:
         card = get_mask_card_number(cards_number[-16:])
-        mask_card = f"{cards_number.replace(cards_number[-16:], card)} "
-        return mask_card
+        masked_payment = f"{cards_number.replace(cards_number[-16:], card)} "
+        return masked_payment
 
 
-def get_data(data: str) -> str | None:
+def get_data(data: str) -> str:
     """Функция преобразования даты"""
     if len(data) < 10:
         return "Некорректный ввод"
